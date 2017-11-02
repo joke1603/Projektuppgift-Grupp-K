0457a8c12a519aa54ddd1d12088b35b68dcf4945
joke1603 <joke1603@COSQ260PC08>
2017-11-02 13:32:51 +0100

    Start

---
+++ Projektuppgift - Grupp K/Projektuppgift - Grupp K/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projektuppgift___Grupp_K.MvcApplication" Language="C#" %>
---
+++ Projektuppgift - Grupp K/Projektuppgift - Grupp K/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projektuppgift___Grupp_K.Startup))]
+namespace Projektuppgift___Grupp_K
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

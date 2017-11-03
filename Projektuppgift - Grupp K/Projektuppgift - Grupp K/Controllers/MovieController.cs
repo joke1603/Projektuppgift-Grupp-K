1514f1d1721bf4ddacdbd1aba3e9a3ac354cc430
haha1604 <haha1604@COSQ260PC07>
2017-11-03 14:34:07 +0100

    Gjort massor med saker

---
+++ Projektuppgift - Grupp K/Projektuppgift - Grupp K/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projektuppgift___Grupp_K.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: Movie 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
